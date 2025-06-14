4deaeac09ef2e8491b05814f2e329b1d6941e283
ivan arroyo <ivanarroyo3083@gmail.com>
2025-06-13 23:22:25 -0300

    se inicio el armado de controladores

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,14 +1,13 @@
 import express from "express"; // importo express
+import { about, getUser, home } from "../controllers/auth.controller.js";
 
 const router = express.Router(); //creo una instancia de express.Router
 
-router.get("/", (req, res) =>{
-    res.end('esta es la ruta para los usuarios')
-})
+router.get("/", home)
 
-router.get("/about", (req, res) =>{
-    res.send('esta es la ruta de about')
-})
+router.get("/about" , about)
+
+router.post("/getUser" , getUser)
 
 
 export default router; //exporto la instancia de express.Router
