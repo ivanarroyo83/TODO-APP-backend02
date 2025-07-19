5c53f312c2c1caa1e43291edddcadea57bf6422d
ivan arroyo <ivanarroyo3083@gmail.com>
2025-07-18 21:11:02 -0300

    profile image hecho

--- src/controllers/profile.controller.js
+++ src/controllers/profile.controller.js
@@ -27,7 +27,7 @@ export const uploadProfileImage = async (req, res)=>{
          if(!user){
             //si no hay usuario debo borrar la imagen que subio
             fs.existsSync(path.join(__dirname, `../../public/uploads/profile/${req.file.filename}`)) && fs.unlinkSync((__dirname, `../../public/uploads/profile/${req.file.filename}`))// si hay un error con el usuario este metodo va a borrar la imagen que se creo
-            
+            return res.status(404).json({message:'usuario no encontrado'})
          }
 
       //si el usuario ya tienen una imagen de perfil la eliminamos
@@ -57,4 +57,30 @@ export const uploadProfileImage = async (req, res)=>{
         res.status(500).json({message:'error al subir la imagen de perfil'});
         
     }
+}
+
+export const getProfileImage = async (req, res) =>{
+    try {
+        
+        const userId = req.user.id; //traigo de la info del usuario a travez del midleware de authrequired quien inyecta la info desde el token
+
+        const user = await User.findById(userId);
+
+        if(!user){
+            return res.status(404).json({message:'usuario no encontrado'})
+        }
+
+        if(!user.profileImage){
+            res.status(404).json({message:'el usuario no tiene fot del perfil'})
+        }
+
+        res.status(200).json({
+            profileImage : user.profileImage
+        })
+
+    } catch (error) {
+     console.log(error);
+     res.status(500).json({message:'error al obtener la imgagen de perfil'})   
+    }
+    
 }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,7 +4,7 @@ import { loginSchema, registerSchema } from "../validators/auth.validator.js";
 import {login,  logout,  profile,  register, verifyEmail, verifyToken } from "../controllers/auth.controller.js";
 import { authRequired } from "../middlewares/validatetoken.js";
 import uploadIconProfileImage from "../helpers/multer.config.iconProfile.js";
-import { uploadProfileImage,  } from "../controllers/profile.controller.js";
+import { getProfileImage, uploadProfileImage,  } from "../controllers/profile.controller.js";
 
 const router = express.Router(); //creo una instancia de express.Router
 
@@ -22,7 +22,7 @@ router.post('/upload-profile-image',
     uploadProfileImage
 );// sube la imagen del perrfil
 
-//router.get('/profile-image')//pide la imagen de perfil
+router.get('/profile-image', authRequired, getProfileImage)//pide la imagen de perfil
 
 
 export default router; //exporto la instancia de express.Router()
